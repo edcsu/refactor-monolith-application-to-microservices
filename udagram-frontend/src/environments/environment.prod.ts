d0792b2d47fd93ba30214ffd9bcacd3fe0b50f31
Keith Ssewanonda <38857594+edcsu@users.noreply.github.com>
2021-08-10 21:29:34 +0300

    feat: add K8s config files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8b9266797c6043f9ab1a9bed3cafeeb-249732980.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
